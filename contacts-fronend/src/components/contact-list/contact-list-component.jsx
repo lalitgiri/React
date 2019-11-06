be090225d29a1f94e616ff78626b84124deedf26
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2019-11-07 00:24:13 +0530

    convert ContactList from controlled  (class) components  to stateless functional components

--- contacts-fronend/src/components/contact-list/contact-list-component.jsx
+++ contacts-fronend/src/components/contact-list/contact-list-component.jsx
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 import './contact-list-component.css';
 
-class ContactList extends Component {
+// functional components ... functional components are stateless...
+function ContactList(props) {
+    const contacts = props.contacts;
 
+    return <ol className='contact-list'>
+        {contacts.map(contact => (
+            <li key={contact.id} className='contact-list-item'>
 
-    render() {
-        const contacts = this.props.contacts;
+                <div className='contact-avatar' style={{
+                    backgroundImage: `url(${contact.avatarURL})`
+                }} />
+                <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.email}</p>
+                </div>
+                <button className='contact-remove'>Remove</button>
+            </li>
+        ))}
+    </ol>;
 
-        return <ol className='contact-list'>
-            {contacts.map(contact => (
-                <li key={contact.id} className='contact-list-item'>
-
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${contact.avatarURL})`
-                    }} />
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button className='contact-remove'>Remove</button>
-                </li>
-            ))}
-        </ol>;
-    }
 }
+
+// controlled components
+
+// class ContactList extends Component {
+
+
+//     render() {
+//         const contacts = this.props.contacts;
+
+//         return <ol className='contact-list'>
+//             {contacts.map(contact => (
+//                 <li key={contact.id} className='contact-list-item'>
+
+//                     <div className='contact-avatar' style={{
+//                         backgroundImage: `url(${contact.avatarURL})`
+//                     }} />
+//                     <div className='contact-details'>
+//                         <p>{contact.name}</p>
+//                         <p>{contact.email}</p>
+//                     </div>
+//                     <button className='contact-remove'>Remove</button>
+//                 </li>
+//             ))}
+//         </ol>;
+//     }
+// }
 
 
 export default ContactList;
