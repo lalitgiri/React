36ca837afdbba7f3a0437bbe32b83c90b63139ad
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2019-11-08 06:05:12 +0530

    implementation of controlled components

--- contacts-frontend/src/components/contact-list/contact-list-component.jsx
+++ contacts-frontend/src/components/contact-list/contact-list-component.jsx
@@ -1,5 +1,70 @@
 import React, { Component } from 'react';
 import './contact-list-component.css';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+class ContactList extends Component {
+
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+    }
+    render() {
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state;
+
+        let showingContacts;
+
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i');
+
+            showingContacts = contacts.filter((contact) => {
+                return match.test(contact.name);
+            });
+        } else {
+            showingContacts = contacts;
+        }
+
+        showingContacts.sort(sortBy('name'));
+        return <div className="list-contacts">
+            {JSON.stringify(this.state)}
+            <div className="list-contacts-top">
+                <input
+                    className="search-contacts"
+                    type="text"
+                    placeholder="Search Contacts"
+                    value={this.state.query}
+                    onChange={(event) => this.updateQuery(event.target.value)} />
+            </div>
+            <ol className='contact-list'>
+                {showingContacts.map(contact => (
+                    <li key={contact.id} className='contact-list-item'>
+
+                        <div className='contact-avatar' style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }} />
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button onClick={() => { onDeleteContact(contact) }} className='contact-remove'>Remove</button>
+                    </li>
+                ))}
+            </ol>
+        </div>;
+    }
+}
+
+
+export default ContactList;
 
 // functional components ... functional components are stateless...
 /*
@@ -24,31 +89,3 @@ function ContactList(props) {
 
 }
 */
-// controlled components
-
-class ContactList extends Component {
-
-
-    render() {
-        const contacts = this.props.contacts;
-
-        return <ol className='contact-list'>
-            {contacts.map(contact => (
-                <li key={contact.id} className='contact-list-item'>
-
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${contact.avatarURL})`
-                    }} />
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button onClick={() => { this.props.onDeleteContact(contact) }} className='contact-remove'>Remove</button>
-                </li>
-            ))}
-        </ol>;
-    }
-}
-
-
-export default ContactList;
