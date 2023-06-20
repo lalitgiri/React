8ee76c2bcc71b3e5b74f9b7b3479b21e87590f12
Lalit Giri <lalitgiri1711@gmail.com>
2023-06-20 06:52:07 +0530

    Reat JS practice with udemy course

---
+++ ReactJS Practice/01-starting-setup/src/components/Header.js
@@ -0,0 +1,11 @@
+function Header({keyConceptsImage}) {
+    return (
+        <header>
+            <img src={keyConceptsImage} alt="Medal badge with a star" />
+            <h1>Key React Concepts</h1>
+            <p>Selected key React concepts you should know about</p>
+        </header>
+    )
+}
+
+export default Header;
